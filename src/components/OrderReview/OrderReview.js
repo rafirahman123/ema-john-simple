4899992a90f9152ef8adf8a1176216d9a1f79e71
rafirahman123 <76124938+rafirahman123@users.noreply.github.com>
2021-09-30 12:39:48 +0600

    hook create

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import useProducts from '../../Hooks/useProducts';
+import useCart from './useCart';
 
 const OrderReview = () => {
+    const [products, setproducts] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
+            <h1>{products.length}</h1>
+            <h2>{cart.length}</h2>
             <h2>This is order reivew</h2>
         </div>
     );
